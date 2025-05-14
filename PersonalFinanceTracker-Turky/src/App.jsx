32a9457b75751cec859e7bf793c692211c9d901d
Tiiilod <turky2202@student.abudzar.sch.id>
2025-05-14 13:23:38 +0700

    add Summary

--- PersonalFinanceTracker-Turky/src/App.jsx
+++ PersonalFinanceTracker-Turky/src/App.jsx
@@ -1,7 +1,23 @@
-export default function App() {
+import { Link } from "react-router-dom";
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold">Finance Tracker</h1>
+      <p className="text-2xl">Welcome to the Finance Tracker App!</p>
+      <Link
+        to="/form"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+      >
+        Add Transaction
+      </Link>
+      <Link
+        to="/summary"
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4"
+      >
+        View Summary
+      </Link>
+    </div>
+  );
 }
+export default App;
